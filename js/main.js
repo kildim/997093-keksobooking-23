bedfb5ee68521ee81a18b3942fe9b16af66551de
Dmitry Kilin <coding@kilin.biz>
2021-05-30 11:56:07 +0300

    implement mentor suggestions

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
-
-/* eslint-disable prefer-const */
-/* eslint-disable no-console */
 const MIN_BORDER_NEGATIVE = 'Нижняя граница диапазона отрицательна';
 const MAX_BORDER_NEGATIVE = 'Верхняя граница диапазона отрицательна';
 const MIN_GRATER_MAX = 'Значение нижней границы диапазона больше верхней границы диапазона';
 const MIN_EQUAL_MAX = 'Значения верхней и нижней границ диапазона совпадают';
 
-const isNegative = (number) => number<0;
-
-const alertIntervalBordersErrors = (validationResult) => {
-  for (let value of validationResult) {
-    console.log(`Ошибка указания границ диапазона: ${  value}`);
+class IntervalBordersError extends Error {
+  constructor(errCode) {
+    super('Ошибка указания границ диапазона');
+    this.name = 'IntervalBordersError';
+    this.errorCode = errCode;
   }
-};
+}
+
+const isNegative = (number) => number < 0;
+
+// const alertIntervalBordersErrors = (validationResults) => {
+//   for (const value of validationResults) {
+//     console.log(`Ошибка указания границ диапазона: ${  value}`);
+//   }
+// };
+
 const validateIntervalBorders = (minBorder, maxBorder) => {
-  let intervalBordersErrors = [];
+  const intervalBordersErrors = [];
 
   if (isNegative(minBorder)) {
     intervalBordersErrors.push(MIN_BORDER_NEGATIVE);
@@ -22,38 +28,48 @@ const validateIntervalBorders = (minBorder, maxBorder) => {
   if (isNegative(minBorder)) {
     intervalBordersErrors.push(MAX_BORDER_NEGATIVE);
   }
-  if (minBorder>maxBorder){
+  if (minBorder > maxBorder){
     intervalBordersErrors.push(MIN_GRATER_MAX);
   }
-  if (minBorder===maxBorder){
+  if (minBorder === maxBorder){
     intervalBordersErrors.push(MIN_EQUAL_MAX);
   }
   return intervalBordersErrors;
 };
-const isIntervalBordersValid = (minBorder, maxBorder) => {
+
+// const isIntervalBordersValid = (minBorder, maxBorder) => {
+//   const intervalBordersErrors = validateIntervalBorders (minBorder, maxBorder);
+//   alertIntervalBordersErrors(intervalBordersErrors);
+//   return !intervalBordersErrors.length;
+// };
+
+const checkIntervalBorders = (minBorder, maxBorder) => {
   const intervalBordersErrors = validateIntervalBorders (minBorder, maxBorder);
-  alertIntervalBordersErrors(intervalBordersErrors);
-  return !intervalBordersErrors.length;
+  if (intervalBordersErrors.length > 0) {
+    throw new IntervalBordersError(intervalBordersErrors);
+  }
+};
+
+const _genNaturalInt = (minBorder, maxBorder) => {
+  minBorder = Math.ceil(minBorder);
+  maxBorder = Math.floor(maxBorder);
+  return (Math.floor(Math.random() * (maxBorder - minBorder + 1)) + minBorder);
+};
+
+const _genUnsignedReal = (minBorder, maxBorder, precision) => {
+  return ((Math.random() * (maxBorder - minBorder)) + minBorder).toFixed(precision);
 };
 
 /* eslint-disable no-unused-vars */
-const getNaturalInt = (minBorder, maxBorder) => {
-  if (isIntervalBordersValid (minBorder, maxBorder)) {
-    minBorder = Math.ceil(minBorder);
-    maxBorder = Math.floor(maxBorder);
-    return (Math.floor(Math.random() * (maxBorder - minBorder + 1)) + minBorder);
-  } else {
-    console.log('!ОШИБКА В УКАЗАНИИ ДИАПАЗОНА!');
-  }
-};
-const setPrecision = (value, precision) => value.toFixed(precision);
-const getUnsignedReal = (minBorder, maxBorder, precision) => {
-  if (isIntervalBordersValid (minBorder, maxBorder)) {
-    return ((Math.random() * (maxBorder - minBorder)) + minBorder).toFixed(precision);
-  } else {
-    console.log('!ОШИБКА В УКАЗАНИИ ДИАПАЗОНА!');
+const getUnsignedNumber = (minBorder, maxBorder, precision = 0) => {
+  try {
+    checkIntervalBorders (minBorder, maxBorder);
+    return precision ? _genUnsignedReal (minBorder, maxBorder, precision) : _genNaturalInt(minBorder, maxBorder) ;
+  } catch (err) {
+    if (err instanceof IntervalBordersError) {
+      // eslint-disable-next-line no-alert
+      alert('!ОШИБКА В УКАЗАНИИ ДИАПАЗОНА!');
+    }
   }
 };
 /* eslint-enable no-unused-vars */
-/* eslint-enable no-console */
-/* eslint-enable prefer-const */
