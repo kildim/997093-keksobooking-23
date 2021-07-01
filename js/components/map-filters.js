fb1fdbfe6d58aa739f7a4f85057f5c9628d6a419
Dmitry Kilin <coding@kilin.biz>
2021-07-01 15:48:53 +0300

    tests passes

--- js/components/map-filters.js
+++ js/components/map-filters.js
@@ -4,11 +4,11 @@ const mapFilters = document.querySelector('.map__filters');
 
 const activateMapFilters = () => {
   activateForm(mapFilters, 'ad-form--disabled');
-}
+};
 
 const deactivateMapFilters = () => {
   deactivateForm(mapFilters, 'map__filters--disabled');
-}
+};
 
 const initMapFilters = () => {
   activateMapFilters(mapFilters, 'map__filters--disabled');
--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-import {mockAd} from './utils/data-factories.js';
-import {renderArticle} from './components/card.js';
+// import {mockAd} from './utils/data-factories.js';
+// import {renderArticle} from './components/card.js';
 import {initAdForm} from './components/ad-form.js';
 import {initMapFilters} from './components/map-filters.js';
 
-const ADS_COUNT = 10;
-const advertisements = new Array(ADS_COUNT).fill(null).map(() => mockAd());
-const blockMap = document.querySelector('#map-canvas');
+// const ADS_COUNT = 10;
+// const advertisements = new Array(ADS_COUNT).fill(null).map(() => mockAd());
+
 
 initAdForm();
 initMapFilters();
