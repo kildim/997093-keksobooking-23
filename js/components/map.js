59ee846218ab20f5ec51b60600e8677bf7eba54e
Dmitry Kilin <coding@kilin.biz>
2021-07-23 00:24:25 +0300

    D14

--- js/components/map.js
+++ js/components/map.js
@@ -13,6 +13,18 @@ const MARKER_ICON_WIDTH = 40;
 const MARKER_ICON_HEIGHT = 40;
 const MARKER_ANCHOR_HORIZONTAL_POSITION = 20;
 const MARKER_ANCHOR_VERTICAL_POSITION = 40;
+const MAIN_MARKER_ICON_OPTIONS =
+  {
+    iconUrl: MAIN_MARKER_ICON,
+    iconSize: [MAIN_ICON_WIDTH, MAIN_ICON_HEIGHT],
+    iconAnchor: [MAIN_ICON_ANCHOR_HORIZONTAL_POSITION, MAIN_ICON_ANCHOR_VERTICAL_POSITION],
+  };
+const COMMON_MARKER_ICON_OPTIONS =
+  {
+    iconUrl: COMMON_MARKER_ICON,
+    iconSize: [MARKER_ICON_WIDTH, MARKER_ICON_HEIGHT],
+    iconAnchor: [MARKER_ANCHOR_HORIZONTAL_POSITION, MARKER_ANCHOR_VERTICAL_POSITION],
+  };
 
 const siteMap = L.map('map-canvas');
 const markers = L.layerGroup();
@@ -21,27 +33,10 @@ const tileLayer = L.tileLayer(
   {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   });
-const mainMarkerIcon = L.icon (
-  {
-    iconUrl: MAIN_MARKER_ICON,
-    iconSize: [MAIN_ICON_WIDTH, MAIN_ICON_HEIGHT],
-    iconAnchor: [MAIN_ICON_ANCHOR_HORIZONTAL_POSITION, MAIN_ICON_ANCHOR_VERTICAL_POSITION],
-  },
-);
-const commonMarkerIcon = L.icon (
-  {
-    iconUrl: COMMON_MARKER_ICON,
-    iconSize: [MARKER_ICON_WIDTH, MARKER_ICON_HEIGHT],
-    iconAnchor: [MARKER_ANCHOR_HORIZONTAL_POSITION, MARKER_ANCHOR_VERTICAL_POSITION],
-  },
-);
-const mainMarker = L.marker (
-  TOKIO_COORDS,
-  {
-    draggable: true,
-    icon: mainMarkerIcon,
-  },
-);
+const mainMarkerIcon = L.icon (MAIN_MARKER_ICON_OPTIONS);
+const commonMarkerIcon = L.icon (COMMON_MARKER_ICON_OPTIONS);
+
+const mainMarker = L.marker (TOKIO_COORDS, {draggable: true, icon: mainMarkerIcon});
 
 /**
  * Создаёт функцию обратного вызова для события 'moveend' главного маркера.
@@ -56,7 +51,7 @@ const onMainMarkerMooveend = (coordsManipulationFunction) => function (evt) {
   const {lat, lng} = evt.target.getLatLng();
   coordsManipulationFunction(lat, lng);
 };
-const _genMarker = (ad) => {
+const genMarker = (ad) => {
 
   const {location: {lat: lat, lng: lng}} = ad;
   const marker = L.marker({lat, lng},{draggable: false, icon: commonMarkerIcon});
@@ -66,7 +61,7 @@ const _genMarker = (ad) => {
 };
 const renderMarkers = (data) => {
   markers.clearLayers();
-  data.forEach(_genMarker);
+  data.forEach(genMarker);
   markers.addTo(siteMap);
 };
 const hidePopups = () => markers.eachLayer((marker) => marker.closePopup());
@@ -84,6 +79,7 @@ const markerMoved = (cb) => {
   mainMarker.on('moveend', onMainMarkerMooveend(cb));
 };
 const resetData = () => {
+  siteMap.setView(TOKIO_COORDS, MAP_SCALE);
   mainMarker.setLatLng(TOKIO_COORDS);
 };
 
